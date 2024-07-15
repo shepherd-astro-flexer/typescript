5b6703b1597b06d06c850fd7cb86918f9fbcd865
flezo19 <78270313+flezo19@users.noreply.github.com>
2024-07-16 00:02:19 +0800

    Initial commit

---
+++ src/tutorial.ts
@@ -0,0 +1,24 @@
+// let name: string = "astro";
+// name = name.toUpperCase();
+// console.log(name);
+
+// let num: number = 1.5435;
+// num = parseFloat(num.toFixed(2));
+// console.log(num);
+
+// let isListening: boolean = false;
+// isListening = true;
+// console.log(isListening);
+
+// let orderStatus: "processing" | "shipped" | "delivered" = "processing";
+// orderStatus = "shipped";
+// orderStatus = "delivered";
+
+// let discount: number | string = 20;
+// discount = "20%";
+
+// let mixed: object[] = [
+//   { id: 1, name: "astro" },
+//   { id: 2, name: "king" },
+// ];
+// console.log(mixed);
